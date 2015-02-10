264fcc333b99217378e52d9eae3799ac1a36ea26
yongbo <wangyongbo90@gmail.com>
2015-02-10 12:24:01 +0800

    学习第五章

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,16 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
 
-  get 'static_pages/help'
+  get 'users/new'
 
-  get 'static_pages/about'
+  get 'home' => 'static_pages#home'
 
-  get 'static_pages/contact'
+  get 'help' => 'static_pages#help'
+
+  get 'about' => 'static_pages#about'
+
+  get 'contact' => 'static_pages#contact'
+
+  get 'signup' => 'users#new'
 
   resources :microposts
 
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -12,7 +12,7 @@ class StaticPagesControllerTest < ActionController::TestCase
   end
 
   test "should get help" do
-    get :help
+    get 'help'
     assert_response :success
     assert_select "title", "Help | #{@base_title}"
   end
